e926fdaa15f75c6b33cd02e260ca3b77c248caa0
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2020-10-08 13:50:53 +0200

    Created reusable VegaLite component

---
+++ components/VegaLite.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="viz"></div>
+</template>
+<script>
+import embed from 'vega-embed'
+export default {
+  props: {
+    chartData: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+    mark: { type: String, default: 'point' },
+    encoding: {
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+  data() {
+    return {
+      def: {
+        $schema: 'https://vega.github.io/schema/vega-lite/v2.0.json',
+        description: 'A simple bar chart with embedded data.',
+        data: {
+          values: this.chartData,
+        },
+        mark: this.mark,
+        encoding: this.encoding,
+      },
+    }
+  },
+  async mounted() {
+    await embed('#viz', this.def, { actions: false })
+  },
+}
+</script>
